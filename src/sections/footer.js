2425080258672ef734edc6b74dbc8606db8bfbbc
tjam2010 <tjam2010@uw.edu>
2020-03-11 03:51:21 -0700

    updating homepage

---
+++ src/sections/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <section className="page" id="footer">
+        <div className="content" id="footer-content">
+          <div id="footer-caption">footer</div>
+        </div>
+      </section>
+    );
+  }
+}
