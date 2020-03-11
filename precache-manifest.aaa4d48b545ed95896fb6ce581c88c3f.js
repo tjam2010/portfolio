4ce4cb407c6576687645c797ea50b1e7c7c5c3cf
tjam2010 <tjam2010@uw.edu>
2020-03-10 22:12:13 -0700

    Updates

---
+++ precache-manifest.aaa4d48b545ed95896fb6ce581c88c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1167a89537a44f5a55985b82109508",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c86f21ddc8fa1886bdf",
+    "url": "/portfolio/static/css/main.9741e8af.chunk.css"
+  },
+  {
+    "revision": "3bc601598dddc1e87633",
+    "url": "/portfolio/static/js/2.0426b651.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.0426b651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c86f21ddc8fa1886bdf",
+    "url": "/portfolio/static/js/main.35172fab.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "fa652feebd191146f5fb237503ca1f85",
+    "url": "/portfolio/static/media/landing.fa652fee.jpg"
+  }
+]);
