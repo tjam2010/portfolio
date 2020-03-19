506911d8d6354dd9973db6577ef57aebf08ffb16
tjam2010 <tjam2010@uw.edu>
2020-03-18 23:12:47 -0700

    favicon, waves, fixed work nav

--- docs/precache-manifest.60bbdf2d9d050630fe159698ef5495a2.js
+++ docs/precache-manifest.60bbdf2d9d050630fe159698ef5495a2.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3bcee05a5d01c48b082de0fa1553f3b0",
+    "revision": "c54efa733f448b8af65891f657270bc2",
     "url": "/index.html"
   },
   {
-    "revision": "ff984a4350c866160fcd",
+    "revision": "85d3eeff79e7a881a707",
     "url": "/static/css/main.9741e8af.chunk.css"
   },
   {
-    "revision": "fdf5c3aee8c5b00888f0",
-    "url": "/static/js/2.d8f2f520.chunk.js"
+    "revision": "bdba3a664104a792465b",
+    "url": "/static/js/2.c936e1d4.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.d8f2f520.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c936e1d4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ff984a4350c866160fcd",
-    "url": "/static/js/main.58da9be8.chunk.js"
+    "revision": "85d3eeff79e7a881a707",
+    "url": "/static/js/main.9a4f5575.chunk.js"
   },
   {
     "revision": "0d46f7656c748826843c",
     "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "013abbb65ef941d3e0a8c1ae852185fb",
+    "url": "/static/media/Resume_TJ_Gascho_12.18.19.013abbb6.pdf"
   },
   {
     "revision": "fa652feebd191146f5fb237503ca1f85",
@@ -30,5 +34,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "07d209eb9888a4aeab13e53babd7824a",
     "url": "/static/media/tj_tedx.07d209eb.jpg"
+  },
+  {
+    "revision": "607a575067f07a2c4da65c457864c125",
+    "url": "/static/media/wave.607a5750.svg"
   }
 ]);
